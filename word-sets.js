912b2574fd3711fae12f20aff18a42e6c94ea862
lokivari <chibitrochan@gmail.com>
2017-11-27 18:43:07 +0800

    Update word-sets.js

--- word-sets.js
+++ word-sets.js
@@ -1,45 +1,48 @@
 // Wrap everything in a function to avoid polluting the global namespace.
 (function() {
-	
-	TypeJig.WordSets['YanhengSingleKeys'] = [
-		"D", "G", "I", "N", "A", "Z", 
-		"W", "U", "E", "O", "B", "X",
-		"-D", "-G", "-I", "-N", "-A", "-Z", 
-		"-W", "-U", "-E", "-O", "-B", "-X"
+
+	TypeJig.WordSets['d-/b-'] = [
+		["dig", "dog", "doff", "due", "dual",
+		"deal", "day", "dad", "date", "dot",
+		"did", "do", "doe", "dove", "down",
+		"death"],
+		["back", "bad", "base", "bass", "boo",
+		"book", "be", "by", "bite", "bog",
+		"bought", "big", "been", "bit", "both",
+		"but"]
 	];
 
-	TypeJig.WordSets['Ch1-words'] = [
-		["得知", "德格", "得以", "德烏", "的恩", "的額", "的啊", "德沃", "得不",
- 		"值得", "制止", "制革", "旨意", "植物", "知恩", "之額", "之啊", "知我", "支部",
-		"歌德", "擱置", "各個", "各異", "歌舞", "戈恩", "個惡", "個啊", "葛沃", "葛布",
-		"醫德", "一直", "一個", "意義", "義務", "一恩", "抑惡", "伊啊", "一窩", "異步",
-		"武德", "物質", "吳哥", "無疑", "無誤", "吳恩", "無惡", "五啊", "無我", "無不",
-		"恩德", "恩之", "恩格", "恩億", "恩物", "恩額", "恩啊", "恩我", "恩不",
-		"額的", "遏制", "額個", "惡意", "訛誤", "厄恩", "鄂啊", "額我", "顎部",
-		"阿德", "啊之", "阿哥", "阿姨", "阿武", "啊恩", "啊額", "啊我", "阿布",
-		"我的", "我支", "我哥", "我一", "我無", "沃恩", "我額", "我啊", "我不",
-		"不得", "佈置", "布格", "不宜", "不無", "布恩", "不額", "不啊", "不我"]
+	TypeJig.WordSets['-ld/-lt'] = [
+		"adult", "asphalt", "assault", "bald", "basalt",
+		"beheld", "behold", "belt", "bold", "bolt",
+		"build", "built", "catapult", "child", "cobalt",
+		"cold", "colt", "consult", "cult", "dealt",
+		"default", "difficult", "dolt", "dwelt", "emerald",
+		"exalt", "exult", "fault", "felt", "field",
+		"fold"
 	];
 
-	TypeJig.WordSets['Ch2-initials'] = [
-		"夫婦", "布鋪", "不睦", "不服", "瀑布", "鋪木", "匍匐",
-		"幕布", "木鋪", "幕府", "腹部", "副鋪", "父母"
+
+	TypeJig.WordSets.nch = [
+		"century", "accentuate", "adventure", "denture", "eventually",
+		"gargantuan", "indenture", "jointure", "venture", "lynch",
+		"avalanche", "bench", "drench", "blanch", "blench",
+		"branch", "brunch", "cinch", "clench", "clinch",
+		"entrench", "finch", "flinch", "grinch", "haunch",
+		"henchmen", "hunch", "linchpin", "munch", "paunch",
+		"pinch", "punch", "quench", "ranch", "raunchy",
+		"scrunch", "squinch", "stanch", "staunch", "tench",
+		"trench", "truncheon", "wench", "winch", "wrench"
 	];
 
-	TypeJig.WordSets.['Ch2-briefs'] = [
-		"不能", "部分", "普遍", "普通", "目的", "目前", "複雜", "負責,"
-		"八", "八月", "。", "，"
-	];
-
-	TypeJig.WordSets['Ch2-comprehensive'] = [
-		"夫婦", "布鋪", "不睦", "不服", "瀑布", "鋪木", "匍匐", "幕布", 
-		"木鋪", "幕府", "腹部", "副鋪", "父母", "不得", "布置", "布格", 
-		"不宜", "不無", "鋪的", "樸直", "譜儀", "穆德", "拇指", "牧歌", 
-		"木屋", "福德", "複製", "弗戈", "複議", "服務", "俯臥", "得不", 
-		"支部", "葛布", "異步", "無不", "顎部", "我不", "質樸", "歌譜", 
-		"臥鋪", "姨母", "烏木", "阿姆", "德芙", "支付", "衣服", "不能", 
-		"部分", "普遍", "普通", "目的", "目前", "複雜", "負責," "八", 
-		"八月", "。", "，", "八部", "八畝"
+	TypeJig.WordSets['-nj'] = [
+		"arrange", "avenge", "binge", "challenge", "change",
+		"cringe", "derange", "exchange", "expunge", "flange",
+		"fringe", "grange", "grunge", "hinge", "impinge",
+		"lounge", "lozenge", "lunge", "mange", "melange",
+		"muskellunge", "orange", "plunge", "range", "revenge",
+		"scavenge", "scrounge", "singe", "sponge", "strange",
+		"syringe", "tinge", "twinge",
 	];
 
 	TypeJig.WordSets['ite/ight'] = [
@@ -5033,4 +5036,4 @@
 		"zones", "zoning", "zonings"
 	];
 
-})();  // Execute the code in the wrapper function.
+})(); // Execute the code in the wrapper function.
